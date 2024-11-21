9cf1f1bfa9dcbb9a334383dd610d3441733d3953
drak138 <bgmingteam@gmail.com>
2024-11-21 19:19:47 +0200

    added images and register
    added images and register plus routing

--- canparo-bank/src/app/app.routes.ts
+++ canparo-bank/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'',component:RegisterComponent,title:"Register Page"},
+    {path:"home",component:HomeComponent, title: "Home Page"}];
