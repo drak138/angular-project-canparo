f73721db5b61015447cd6d7ed4775186c660550d
drak138 <bgmingteam@gmail.com>
2024-11-22 10:47:53 +0200

    added cards and cards details

--- canparo-bank/src/app/app.routes.ts
+++ canparo-bank/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { AboutComponent } from './components/about/about.component';
 import { PartnersComponent } from './components/partners/partners.component';
 import { BalanceComponent } from './components/balance/balance.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { CardDetailsComponent } from './components/card-details/card-details.component';
 
 export const routes: Routes = [{
     path:'',component:LoginComponent,title:"Login Page"},
@@ -16,6 +18,11 @@ export const routes: Routes = [{
     {path:'about/partners',component:PartnersComponent, title: "Partners Page",},
     {path:'about/contacts',component:ContactsComponent, title: "Contacts Page",},
     {path:'card/balance',component:BalanceComponent,title:"My Balance"},
+    {path:'card',children:[
+        {path:'myCards',component:CardsComponent},
+        {path:':id/details',component:CardDetailsComponent}
+    ]
+}
 
 ];
     
