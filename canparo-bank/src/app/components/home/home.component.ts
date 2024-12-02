f4816075b28e4dc4eacac24aee1d6aac43e94bb7
drak138 <bgmingteam@gmail.com>
2024-12-03 01:35:53 +0200

    added home nav

--- canparo-bank/src/app/components/home/home.component.ts
+++ canparo-bank/src/app/components/home/home.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  currentSlide = 0;
 
+  navigate(direction: 'left' | 'right'): void {
+    if (direction === 'left') {
+      this.currentSlide = (this.currentSlide - 1 + 3) % 3; // Wrap around to the last slide
+    } else {
+      this.currentSlide = (this.currentSlide + 1) % 3; // Wrap around to the first slide
+    }
+  }
 }
